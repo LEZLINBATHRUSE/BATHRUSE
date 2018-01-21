baa3d33b42a3ffee8b4281a7f2d1fea9e654d2ed
LEZLINBATHRUSE <35652581+LEZLINBATHRUSE@users.noreply.github.com>
2018-01-21 16:23:16 +0530

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("Enter the value of a&b&c");
+scanf("%d\n%d\n%d",&a,&b,&c);
+if(a>b>c)
+{
+    printf("a is greater");
+}
+else
+{
+    printf("a is not greater");
+}
+}
