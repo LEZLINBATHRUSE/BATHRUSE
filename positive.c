7287db1cdc2e05807f16c07785b7bbb4d8efb010
LEZLINBATHRUSE <35652581+LEZLINBATHRUSE@users.noreply.github.com>
2018-01-21 15:01:38 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int N;
+clrscr();
+printf("Enter the value of N");
+scanf("%d",&N);
+if(1<=N<=100000)
+{
+printf("Positive");
+}
+else if
+{
+printf("Negative");
+}
+else
+{
+printf("Zero");
+}
+getch();
+}
