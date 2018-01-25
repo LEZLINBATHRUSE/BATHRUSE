85304ba1c409b2b7e22e636e750372d8ff9c38fe
LEZLINBATHRUSE <35652581+LEZLINBATHRUSE@users.noreply.github.com>
2018-01-25 21:08:58 +0530

    Update 9.c

--- 9.c
+++ 9.c
@@ -2,9 +2,8 @@
 void main()
 {
 int n,i,K=0;
+  printf("\n Sum of 1 2 3 4 5");
 printf(" \n ENTER THE N VALUE");
-printf("\n 1 2 3 4 5");
-printf(" \nENTER THE K VALUE");
 scanf("%d",&n);
 for(i=1;i<=n;++i)
 {
