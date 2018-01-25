6c681e48265568fd02a6069ecce20cc401494f1f
LEZLINBATHRUSE <35652581+LEZLINBATHRUSE@users.noreply.github.com>
2018-01-25 21:13:17 +0530

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter the value of a");
+    scanf("%d",&a);
+    printf("Enter the value of b");
+    scanf("%d",&b);
+    c=pow(a,b);
+    printf("The value of is = %d",c);
+    }
