0e9c40a28e45df5ebfd08ade9abe138b4e3140a8
LEZLINBATHRUSE <35652581+LEZLINBATHRUSE@users.noreply.github.com>
2018-01-30 15:47:42 +0530

    Create p2.c

---
+++ p2.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+void main()
+{
+int a,i;
+unsigned long long factorial = 1;
+printf("Enter an integer: ");
+scanf("%d",&a);
+if (a< 0)
+printf("Error! Factorial of a negative number doesn't exist.");
+else
+{
+for(i=1;i<=a;++i)
+{
+factorial *= i;              
+}
+printf("Factorial of %d = %llu", a, factorial);
+}
+return 0;
+}
