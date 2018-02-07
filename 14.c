c0fc21ebd1274f7b9f5ae447ffe1aa81d9206b0b
LEZLINBATHRUSE <35652581+LEZLINBATHRUSE@users.noreply.github.com>
2018-02-07 19:58:31 +0530

    Update 14.c

--- 14.c
+++ 14.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 void main()
 {
-int i,a=0,s=6;
-    for(int i=a+1;i<s;i++)
+int i;
+    for(i=1;i<6;i++)
     {
-        if(i%2==0)
+        if(i%2==0&&i!=1)
         {
             printf("%d ",i);
         }
