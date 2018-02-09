9acad24ec91b3664cf1679d1eeb064a9e4680551
LEZLINBATHRUSE <35652581+LEZLINBATHRUSE@users.noreply.github.com>
2018-02-09 16:36:01 +0530

    Create 29.c

---
+++ 29.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+      int a[1],b,i=0;
+    printf("Enter the value");
+    scanf("\n%d",&b);
+    {
+        scanf("\n%d",&a[i]);
+        a[i]=b;
+printf("\n%d\t%d",i,a[i]);
+}
+}
+
