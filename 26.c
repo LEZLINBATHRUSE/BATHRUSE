713868734df583647f39307e519a8e597d71f887
LEZLINBATHRUSE <35652581+LEZLINBATHRUSE@users.noreply.github.com>
2018-02-12 06:15:02 +0530

    Create 26.c

---
+++ 26.c
@@ -0,0 +1,33 @@
+
+#include<stdio.h>
+void main()
+{
+int b[50];
+int i,j,c;
+printf("Enter the array elements");
+for(i=0;i<3;i++)
+{
+scanf("%d",&b[i]);
+}
+printf("The sorted array ");
+for(i=0;i<3;i++)
+{
+for(j=i+1;j<3;j++)
+{
+if(b[i]>b[j])
+{
+c=b[i];
+b[i]=b[j];
+b[j]=c;
+}
+}
+}
+for(i=0;i<3;i++)
+{
+printf("\nThe array of sorted elements is %d",b[j]);
+}
+for(i=0;i<3;i++)
+{
+printf("\n%d",b[1]);
+}
+}
