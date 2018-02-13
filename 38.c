74c9a9541c71222db160b82110708ccfa73f6f02
LEZLINBATHRUSE <35652581+LEZLINBATHRUSE@users.noreply.github.com>
2018-02-13 15:57:12 +0530

    Update 38.c

--- 38.c
+++ 38.c
@@ -4,9 +4,9 @@ void main()
 int a,b;
 printf("Enter a & b value");
 scanf("%d\t%d",&a,&b);
-printf("Swapping 2numbers");
+printf("\nSwapping 2numbers");
 a=a^b;
 b=b^a;
 a=b^a;
-printf("A=%d &B=%d",a,b);
+printf("\nA=%d \nB=%d",a,b);
 }
