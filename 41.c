9b48e81cf61b27b92e8fb0cab1ffabf7e6fdd23a
LEZLINBATHRUSE <35652581+LEZLINBATHRUSE@users.noreply.github.com>
2018-02-13 21:37:55 +0530

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n,i,c;
+char a[20];
+printf("Enter the string");
+scanf("%s",&a);
+printf("Enter n value");
+scanf("%d",&n);
+while(i<n)
+{
+printf("%S\n",a[i]);
+c++;
+}
+}
