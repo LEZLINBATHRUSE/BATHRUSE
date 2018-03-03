7b15bb4152cfd82553a972f36bc67fbcd0c4b10f
LEZLINBATHRUSE <35652581+LEZLINBATHRUSE@users.noreply.github.com>
2018-03-03 16:43:34 +0530

    Create p5.c

---
+++ p5.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+void main()
+{
+char rom[100]={I,II,III,IV,V,VI,VII,VIII,IX,X};
+switch(rom)
+{
+case I:
+printf("\n1");
+break;
+case II:
+printf("\n2");
+break;
+case III:
+printf("\n3");
+break;
+case IV:
+printf("\n4");
+break;
+case V:
+printf("\n5");
+break;
+case VI:
+printf("\n6");
+break;
+case VII:
+printf("\n7");
+break;
+case VIII:
+printf("\n8");
+break;
+case IX:
+printf("\n9");
+break;
+case X:
+printf("\n10");
+break;
+default:
+printf("Enter the roman no");
+break;
+}
+}
