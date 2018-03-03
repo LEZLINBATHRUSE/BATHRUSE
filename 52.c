8b9aa3a8cfc3c8fd78ddfe9472e6e208c7fdcd28
LEZLINBATHRUSE <35652581+LEZLINBATHRUSE@users.noreply.github.com>
2018-03-03 13:06:14 +0530

    Create 52.c

---
+++ 52.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+void main()
+{
+int no;
+printf("Enter no from 1 to 10");
+scanf("\n%d",&no);
+switch(no)
+{
+case 1:
+printf("\nOne");
+break;
+case 2:
+printf("\nTwo");
+break;
+case 3:
+printf("\nThree");
+break;
+case 4:
+printf("\nFour");
+break;
+case 5:
+printf("\nFive");
+break;
+case 6:
+printf("\nSix");
+break;
+case 7:
+printf("\nSeven");
+break;
+case 8:
+printf("\nEight");
+break;
+case 9:
+printf("\nNine");
+break;
+case 10:
+printf("\nTen");
+break;
+default:
+printf("\nEnter the number first");
+break;
+}
+}
+
+
+
+
+
