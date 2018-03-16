fc72dbda653a4c6d62375c3a41bf05e46a5c0c54
LEZLINBATHRUSE <35652581+LEZLINBATHRUSE@users.noreply.github.com>
2018-03-16 14:19:12 +0530

    Create 62.c

---
+++ 62.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int a[10],i,n;
+printf("Enter n value");
+scanf("\n%d",&n);
+printf("\nEnter a values");
+for(i=0;i<n;i++)
+{
+scanf("\n%d",&a[i]);
+}
+if((a[i]=0)||(a[i]=1))
+{
+printf("\nYes");
+}
+else
+{
+printf("No");
+}
+}
