97e84ab3e906facb7d59e577a5d004bd2c231be2
LEZLINBATHRUSE <35652581+LEZLINBATHRUSE@users.noreply.github.com>
2018-03-16 14:30:03 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("Enter a value");
+scanf("\n%d",&a);
+printf("\nEnter b value");
+scanf("\n%d",&b);
+printf("\nEnter c value");
+scanf("\n%d",&c);
+if(b<a<c)
+{
+printf("\nYes");
+}
+else
+{
+printf("\nNo");
+}
+}
